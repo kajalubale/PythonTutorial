fb79246dadd98f39f1bf3f860f9badeeecc63c95
kajalubale <67957926+kajalubale@users.noreply.github.com>
2020-07-11 22:47:27 +0530

    Add files via upload

--- Exercise4-Astrologer star.py
+++ Exercise4-Astrologer star.py
@@ -26,20 +26,35 @@
 # *
 
 # Solve Here :
-print("pattern printing")
-while(True):
-    num=int(input("Enter number how many rows you want : "))
-    print("enter 1 or 0 :")
-    bool_val= input("1 for True or 0 for False :")
+# print("pattern printing")
+# while(True):
+#     num=int(input("Enter number how many rows you want : "))
+#     print("enter 1 or 0 :")
+#     bool_val= input("1 for True or 0 for False :")
+#
+#     if bool_val=="1":
+#             pattern =  True
+#             if pattern:
+#                 for i in range(0,num+1):  # start  from 0 and end at num+1
+#                     print("*"*int(i))
+#     if bool_val=="0":
+#             pattern = False
+#             if not pattern:
+#                 for i in range(num,0,-1):  # start  from num and end at 0 ,for reverse -1
+#                     print("*"*int(i))
+#     continue
 
-    if bool_val=="1":
-            pattern =  True
-            if pattern:
-                for i in range(0,num+1):  # start  from 0 and end at num+1
-                    print("*"*int(i))
-    if bool_val=="0":
-            pattern = False
-            if not pattern:
-                for i in range(num,0,-1):  # start  from num and end at 0 ,for reverse -1
-                    print("*"*int(i))
-    continue
+a=int(input("please add number of line you want to print"))
+b=bool(int(input("please add 0 for false")))
+
+def star(a,b):
+    if b==True:
+        c=1
+        while c<=a:
+            print(c * "*")
+            c=c+1
+    else:
+        while a>0:
+            print(a* "*")
+            a=a-1
+star(a,b)
